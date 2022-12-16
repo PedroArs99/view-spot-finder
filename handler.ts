033ab2953289a5ef20dece9a033fb611ecb743dd
Pedro Arenas <pedroars99@icloud.com>
2022-12-16 17:55:48 +0100

    Use a typescript handler

--- handler.ts
+++ handler.ts
@@ -1,12 +1,13 @@
 'use strict';
 
-module.exports.viewSpotFinder = async (event) => {
+export async function viewSpotFinder(event) {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v3.0! Your function executed successfully!',
         input: event,
+        context,
       },
       null,
       2
