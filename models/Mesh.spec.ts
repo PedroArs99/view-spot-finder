b3e9b6a30c1f6e8906d9ff73ce80c3b380128eae
Pedro Arenas <pedroars99@icloud.com>
2022-12-20 19:25:14 +0100

    Add Mesh unit tests

---
+++ models/Mesh.spec.ts
@@ -0,0 +1,197 @@
+import mesh1 from "../examples/mesh_1.json";
+import mesh2 from "../examples/mesh_2.json";
+import mesh3 from "../examples/mesh_3.json";
+import mesh200 from "../examples/mesh_200.json";
+import { MeshDto, meshDtoToDomain } from "./MeshDto";
+
+describe("Mesh Tests", () => {
+  it.each([-1, 0])("Should return empty array", (n: number) => {
+    const mesh = meshDtoToDomain(mesh200.mesh);
+
+    const result = mesh.findNViewSpots(n);
+
+    expect(result.length).toBe(0);
+  });
+
+  it.each([1, 2, 100])(
+    "Should return the only item on the mesh",
+    (n: number) => {
+      const mesh = meshDtoToDomain(mesh1.mesh);
+
+      const result = mesh.findNViewSpots(n);
+
+      const expectedResult = {
+        id: 0,
+        nodes: [
+          {
+            id: 0,
+            x: 0.0,
+            y: 0.0,
+          },
+          {
+            id: 1,
+            x: 0.0,
+            y: 1.0,
+          },
+          {
+            id: 12,
+            x: 1.0,
+            y: 1.0,
+          },
+        ],
+        value: 0.15154957113761364,
+      };
+      expect(result.length).toBe(1);
+      expect(result[0]).toEqual(expectedResult);
+    }
+  );
+
+  it.each([mesh2.mesh, mesh3.mesh])(
+    "Should return just one element",
+    (meshDto: MeshDto) => {
+      const mesh = meshDtoToDomain(meshDto);
+
+      const result = mesh.findNViewSpots(2);
+
+      const expectedResult = {
+        id: 0,
+        nodes: [
+          {
+            id: 0,
+            x: 0.0,
+            y: 0.0,
+          },
+          {
+            id: 1,
+            x: 0.0,
+            y: 1.0,
+          },
+          {
+            id: 12,
+            x: 1.0,
+            y: 1.0,
+          },
+        ],
+        value: 0.15154957113761364,
+      };
+      expect(result.length).toBe(1);
+      expect(result[0]).toEqual(expectedResult);
+    }
+  );
+
+  it("Should return the first 5 results", () => {
+    const mesh = meshDtoToDomain(mesh200.mesh);
+
+    const result = mesh.findNViewSpots(5);
+
+    const expectedResult = [
+      {
+        id: 153,
+        nodes: [
+          {
+            id: 83,
+            x: 7,
+            y: 6,
+          },
+          {
+            id: 94,
+            x: 8,
+            y: 6,
+          },
+          {
+            id: 95,
+            x: 8,
+            y: 7,
+          },
+        ],
+        value: 5.99412916855438,
+      },
+      {
+        id: 141,
+        nodes: [
+          {
+            id: 77,
+            x: 7,
+            y: 0,
+          },
+          {
+            id: 88,
+            x: 8,
+            y: 0,
+          },
+          {
+            id: 89,
+            x: 8,
+            y: 1,
+          },
+        ],
+        value: 5.59673083328692,
+      },
+      {
+        id: 99,
+        nodes: [
+          {
+            id: 53,
+            x: 4,
+            y: 9,
+          },
+          {
+            id: 64,
+            x: 5,
+            y: 9,
+          },
+          {
+            id: 65,
+            x: 5,
+            y: 10,
+          },
+        ],
+        value: 3.7165791791790643,
+      },
+      {
+        id: 87,
+        nodes: [
+          {
+            id: 47,
+            x: 4,
+            y: 3,
+          },
+          {
+            id: 58,
+            x: 5,
+            y: 3,
+          },
+          {
+            id: 59,
+            x: 5,
+            y: 4,
+          },
+        ],
+        value: 3.6258426752667208,
+      },
+      {
+        id: 199,
+        nodes: [
+          {
+            id: 108,
+            x: 9,
+            y: 9,
+          },
+          {
+            id: 119,
+            x: 10,
+            y: 9,
+          },
+          {
+            id: 120,
+            x: 10,
+            y: 10,
+          },
+        ],
+        value: 2.047341538506613,
+      },
+    ];
+    expect(result.length).toBe(5);
+    expect(result).toEqual(expectedResult);
+  });
+});
